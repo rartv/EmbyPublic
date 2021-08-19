20b4bfa4102c37b37e5ca6518136430224e907a6
RARTV <72600814+rartv@users.noreply.github.com>
2021-08-19 15:14:38 +0800

    Create rename.js

---
+++ surge/rename.js
@@ -0,0 +1,28 @@
+if ($request.url.indexOf('/Videos/') != -1) {
+  if($response.status==200){
+    let query = getQueryVariable($request.url);
+    $response.headers['Content-Disposition'] = 'attachment;filename=' + decodeURI(query.filename);
+    $done({
+      status: 200,
+      headers: $response.headers
+    })
+  }else{
+    $done({});
+  }
+}
+
+function getQueryVariable(url){
+  let index=url.lastIndexOf('?');
+  let query = url.substring(index+1, url.length);
+  let vars = query.split("&");
+  let querys = new Object();
+  for (let i=0; i<vars.length; i++) {
+    let pair = vars[i].split("=");
+    querys[pair[0]] = pair[1]
+  }
+  if (Object.keys(querys).length == 0) {
+    return null;
+  }else{
+    return querys;
+  }
+}
