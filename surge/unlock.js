eb55ecf453847f4aa7ebfb70d8ce72ad9e986099
RARTV <72600814+rartv@users.noreply.github.com>
2021-08-23 20:32:12 +0800

    Update unlock.js

--- surge/unlock.js
+++ surge/unlock.js
@@ -1,3 +1,17 @@
+/**
+*
+* @Title: unlock.js
+*
+* @Description: 客户端播放权限解锁
+*
+* @author: Arcade
+*
+* @version V1.0
+*
+* @Copyright: 2021 https://t.me/EmbyPublic All rights reserved.
+*
+*/
+
 if ($request.url.indexOf('mb3admin.com/admin/service/registration/validateDevice') != -1) {
   if($response.status!=200){
       // $notification.post("播放权限已激活", "", "");
