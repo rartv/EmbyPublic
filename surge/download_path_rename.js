d2a1d27645b16b1788423a7f503dc583df31ffe8
RARTV <72600814+rartv@users.noreply.github.com>
2021-09-26 23:56:08 +0800

    支持伪路径的字幕

--- surge/download_path_rename.js
+++ surge/download_path_rename.js
@@ -11,7 +11,18 @@
 * @Copyright: 2021 https://t.me/EmbyPublic All rights reserved.
 *
 */
-
+if ($request.url.indexOf('/Videos/') != -1 && $request.url.indexOf('/Subtitles/') != -1 && $request.method == 'GET') {
+    let query = getQueryVariable($request.url);
+    if (typeof(query.sub_codec) == "undefined" && query.sub_codec == "" && typeof(query.filename) == "undefined" || query.filename == "") {
+        $done({});
+    }
+    $request.url = $request.url.replace(query.filename, 'Stream.' + query.sub_codec);
+    console.log($request.url);
+    $done({
+        url: $request.url,
+        headers: $request.headers
+    });
+}
 if ($request.url.indexOf('/Videos/') != -1 && $request.method == 'GET') {
     $request.url = $request.url.replace(/\/stream\/.+\?mediaSourceId/, '/stream\?mediaSourceId');
     $done({
@@ -20,4 +31,20 @@ if ($request.url.indexOf('/Videos/') != -1 && $request.method == 'GET') {
     });
 }else{
     $done({});
+}
+
+function getQueryVariable(url){
+  let index=url.lastIndexOf('?');
+  let query = url.substring(index+1, url.length);
+  let vars = query.split("&");
+  let querys = new Object();
+  for (let i=0; i<vars.length; i++) {
+    let pair = vars[i].split("=");
+    querys[pair[0]] = pair[1]
+  }
+  if (Object.keys(querys).length == 0) {
+    return null;
+  }else{
+    return querys;
+  }
 }
