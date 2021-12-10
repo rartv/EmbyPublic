679158531045c750f03010536555acffb1368ddc
RARTV <72600814+rartv@users.noreply.github.com>
2021-12-11 02:10:24 +0800

    Update emby-plugin.js

--- surge/emby-plugin.js
+++ surge/emby-plugin.js
@@ -20,7 +20,7 @@ if(requestURL.indexOf(addLink) != -1){  // 添加外部播放器链接
   if(obj.MediaSources){
     obj.MediaSources.forEach((item, index) => {
       let fileName = item['Path'].substring(item['Path'].lastIndexOf('/') + 1);
-      let videoUrl = host + '/Videos/'+ obj.Id +'/stream/' + encodeURIComponent(fileName) + '?DeviceId='+ query['X-Emby-Device-Id'] +'&MediaSourceId='+ item.Id +'&Static=true&api_key='+ query['X-Emby-Token'] + '&filename=' + encodeURIComponent(fileName);
+      let videoUrl = host + '/Videos/'+ obj.Id +'/stream/' + encodeURIComponent(fileName) + '?MediaSourceId='+ item.Id +'&Static=true&api_key='+ query['X-Emby-Token'] + '&filename=' + encodeURIComponent(fileName);
       let shuInfo = [{
         'header': {
           'User-Agent': 'Download',
